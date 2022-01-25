3e621bc11f91b16a93b8777c09ca769aac1fe67a
Hannah Samuel <90801601+hannahsamuel@users.noreply.github.com>
2022-01-24 22:05:06 -0800

    Update Activity3Tester.java

--- 2048 Student Version 2021/src/Activity3Tester.java
+++ 2048 Student Version 2021/src/Activity3Tester.java
@@ -4,7 +4,7 @@ public class Activity3Tester {
 	public static void main(String[] args) {
 		
 		//Declare a Board object
-		
+		Board b = new Board();
 		//create an array with some values you want to 
 		//slideRight. ie [2 0 0 0]
 		
@@ -13,8 +13,14 @@ public class Activity3Tester {
 		
 		//print out the array to see if slideRight
 		//worked
+		int[] array = {2,0,0,2};
+		//System.out.print(b);
+		b.slideRight(array);
+		//System.out.print(array);
+		for(int val : array) {
+			System.out.println(val);
 		
-		
+		}
 	}
 
 }
