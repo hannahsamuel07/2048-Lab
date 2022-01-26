3490cced896a62a3641a26f09df327c8126e613d
Hannah Samuel <90801601+hannahsamuel@users.noreply.github.com>
2022-01-25 22:31:50 -0800

    Update BoardGUI.java

--- 2048 Student Version 2021/src/BoardGUI.java
+++ 2048 Student Version 2021/src/BoardGUI.java
@@ -101,19 +101,19 @@ public class BoardGUI extends JPanel implements KeyListener, ActionListener{
 			
 			//slide right
 			case 39:
-				data.slideRight();
+				data.right();
 				break;
 				
 			case 37: //left
-				data.slideLeft();
+				data.left();
 				break;
 			case 38: //up
 				//what to do if keyCode is 38?
-				
+				data.up();
 				
 				break;
 			case 40: //down
-				
+				data.down();
 				break;
 		}
 		
